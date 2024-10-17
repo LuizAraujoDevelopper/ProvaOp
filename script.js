82349a6c5b31a75939feb520f32792f74e58148a
LuizAraujoDevelopper <126970799+LuizAraujoDevelopper@users.noreply.github.com>
2024-10-17 09:57:27 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,51 @@
+// Descrição: Script para a calculadora
+/**
+ * Neste exercício você deverá implementar a lógica de uma calculadora (com JS).
+ * Para isso é provido um conjunto de arquivos-base e você deverá trabalhar no somente
+ * no arquivo "script.js". Ele contém comentários indicando o que deve ser feito, mas vocês
+ * são livres para modificar e adotar uma estrutura própria.
+ *
+ * A calculadora deve possuir as seguintes funcionalidades:
+ * - Adição de números
+ * - Subtração de números
+ * - Multiplicação de números
+ * - Divisão de números
+ * - Limpar o visor
+ * - Inverter o sinal do número
+ * - Deletar o último caractere
+ * - Exibir o resultado da operação
+ * - Evitar que o usuário digite dois operadores seguidos
+ * - Evitar que o usuário digite um operador no início da operação
+ * - Evitar que o usuário divida um número por zero
+ * - Evitar que o usuário digite mais de um ponto em um número
+ * - Evitar que o usuário digite mais de um zero no início de um número
+ * - Evitar que o usuário digite mais de um operador no início da operação
+ * - Evitar que o usuário digite mais de um operador no final da operação
+ */
+
+// Função que adiciona um número ou operador ao visor da calculadora
+//TODO: Implemente a lógica da função
+
+// Função que verifica se o usuário digitou mais de um operador seguido
+//TODO: Implemente a lógica da função
+
+// Função que verifica se o usuário digitou mais de um operador seguido
+//TODO: Implemente a lógica da função
+
+// Função que verifica se o usuário digitou mais de um ponto em um número
+// TODO: Implemente a lógica da função
+
+// Função que verifica se o usuário digitou mais de um zero no início de um número
+// TODO: Implemente a lógica da função
+
+// Função que verifica se o usuário digitou mais de um operador no início da operação
+// TODO: Implemente a lógica da função
+
+// Função que verifica se o usuário digitou mais de um operador no final da operação
+// TODO: Implemente a lógica da função
+
+// Função que gerencia os eventos de clique nos botões da calculadora
+// TODO: Implemente a lógica da função
+
+// Função que gerencia os eventos de teclado na calculadora
+//TODO: Implemente a lógica da função
